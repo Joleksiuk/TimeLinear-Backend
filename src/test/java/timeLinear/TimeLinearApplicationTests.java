c9956a43af987e9945c3b2b6449b80ce7b1c3cc6
Julia Oleksiuk <julkaoleksiuk26@gmail.com>
2023-10-08 20:23:42 +0200

    Initial commit

---
+++ src/test/java/timeLinear/TimeLinearApplicationTests.java
@@ -0,0 +1,13 @@
+package timeLinear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeLinearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
